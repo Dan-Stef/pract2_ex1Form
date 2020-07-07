ccd0a9cc5d362b805fb4234bac32c5a8933c1742
Даниил Стефаненков <daniilstef2391@gmail.com>
2020-07-08 01:47:30 +0300

    Добавьте файлы проекта.

---
+++ pract2_Form/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace pract2_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(16, 114);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(114, 29);
+            this.textBox3.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(18, 393);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 45);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Проверить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(155, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(642, 435);
+            this.pictureBox.TabIndex = 11;
+            this.pictureBox.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(55, 54);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(60, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(55, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(60, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(14, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 24);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Y";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "X";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ pract2_Form/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pract2_Form
+{
+    public partial class Form1 : Form
+    {
+        private const int PIX_IN_ONE = 15;
+        private const int RADIUS = 12 * PIX_IN_ONE;
+        private const int Ox = 22 * PIX_IN_ONE;
+        private const int Oy = 14 * PIX_IN_ONE; 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                if (textBox1.Text != "" && isValid(textBox1.Text) && textBox2.Text != "" && isValid(textBox2.Text))
+                draw(textBox1.Text, textBox2.Text);
+        }
+        private bool isValid(String str)
+        {
+            char[] chArr = str.ToCharArray();
+            for (int i = 0; i < chArr.Length; i++)
+            {
+                if (!(chArr[i] >= '0' && chArr[i] <= '9') && chArr[i] != '-')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private void draw(string text1, string text2)
+        {
+            Bitmap btm = new Bitmap(pictureBox.Width, pictureBox.Height);
+            Graphics graphics = Graphics.FromImage(btm);
+            Pen p = new Pen(Brushes.Black);
+            Pen p2 = new Pen(Brushes.DarkGray);
+
+            int v = 0;
+            while (v < pictureBox.Width)
+            {
+                graphics.DrawLine(p2, v, 0, v, pictureBox.Height);
+                v += PIX_IN_ONE / 2;
+            }
+         
+            int g = 0;
+            while (g < pictureBox.Height)
+            {
+                graphics.DrawLine(p2, 0, g, pictureBox.Width, g);
+                g += PIX_IN_ONE / 2;
+            }
+
+          
+
+     
+            graphics.DrawLine(p, 22 * PIX_IN_ONE, 0, 22 * PIX_IN_ONE, pictureBox.Height);
+
+        
+            graphics.DrawLine(p, 0, 14 * PIX_IN_ONE, pictureBox.Width, 14 * PIX_IN_ONE);
+
+         
+            graphics.DrawEllipse(p, 14 * PIX_IN_ONE + PIX_IN_ONE, 8 * PIX_IN_ONE - PIX_IN_ONE, RADIUS + PIX_IN_ONE + PIX_IN_ONE / 2, RADIUS + PIX_IN_ONE + PIX_IN_ONE / 2);
+
+   
+            graphics.DrawLine(p, Ox, Oy, 30 * PIX_IN_ONE, 6 * PIX_IN_ONE);
+            graphics.DrawLine(p, Ox, Oy, 14 * PIX_IN_ONE, 6 * PIX_IN_ONE);
+
+            int x1 = Ox, y1 = Oy;
+            double x11 = 0, y11 = 0;
+            int x2 = Ox, y2 = Oy;
+            double x22 = 0, y22 = 0;
+            while (x11 * x11 + y11 * y11 < 49)
+            {
+                while (x22 * x22 + y22 * y22 < 49)
+                {
+                    x2 -= PIX_IN_ONE / 3;
+                    y2 -= PIX_IN_ONE / 3;
+                    x22 -= 0.3333;
+                    y22 -= 0.3333;
+
+                }
+                graphics.DrawLine(p, x1, y1, x2, y2);
+                x1 += PIX_IN_ONE / 2;
+                y1 -= PIX_IN_ONE / 2;
+                x11 += 0.5;
+                y11 -= 0.5;
+
+                x2 = x1;
+                y2 = y1;
+                x22 = x11;
+                y22 = y11;
+
+            }
+
+
+            graphics.FillEllipse(Brushes.Red, Ox + (Convert.ToInt32(text1) * (PIX_IN_ONE / 2)) - 5, Oy - (Convert.ToInt32(text2) * (PIX_IN_ONE / 2 )) - 5, 10, 10);
+            textBox3.Text = Check(float.Parse(text1), float.Parse(text2));
+
+            pictureBox.Image = btm;
+        }
+        private String Check(float x, float y)
+        {
+            if (x * x + y * y < 15 * 15 && y > Math.Abs(x))
+                return "Да";
+            else if (x * x + y * y <= 15 * 15 && y == Math.Abs(x) || x * x + y * y == 15 * 15 && y > Math.Abs(x))
+                return "На границе";
+            else return "Нет";
+
+        }
+    }
+}
